29ee26acef49381e6a638eded42e2697d53f85ae
Nicolas Couvrat <nicolas.couvrat@datadoghq.com>
2019-06-04 18:32:25 -0400

    add basic cli entrypoint

---
+++ nes.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "console.h"
+#include "logger.h"
+
+
+int main(int argc, char* argv[]) {
+  Logger log = Logger::getLogger("main");
+  if (argc != 2) {
+    log.error() << "Oops, path to a .nes file was not provided\n";
+    return -1;
+  } 
+  std::string path(argv[1]);
+  Console console(path);
+  while (console.isRunning()) {
+    console.step();
+  }
+  return 0;
+}
